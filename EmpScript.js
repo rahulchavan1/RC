3de2eb83642e600203e3c9f567fdc9429c46dbd0
rahulchavan1 <32034335+rahulchavan1@users.noreply.github.com>
2020-02-05 15:04:05 +0530

    Add files via upload

--- EmpScript.js
+++ EmpScript.js
@@ -20,20 +20,27 @@
     }
 
     function Listitr(userList){
-        var list = "<th>Id</th><th>Name</th><th>Username</th><th>Email</th><th>Company</th><th>Edit/Delete</th>";
+        
+        var list = "<thead><tr><th onclick='sortTable()'>Id</th><th>Name</th><th>Username</th><th>Email</th><th>Company</th><th>Edit/Delete</th></tr></thead>";
         for(i=0;i<userList.length;i++){
-            list += `<tr> <td> ${userList[i].id} </td> <td> ${userList[i].name} </td> <td> ${userList[i].username} </td> <td> ${userList[i].email} </td> 
-            <td> ${userList[i].company.name} </td> <td><button class = "btn btn-primary" onclick="editUserWindow(${userList[i].id})">Edit</button>&nbsp;&nbsp;<button class="btn btn-danger" onclick = "deleteUser(${userList[i].id})">Delete</button> </td></tr>`;
+            list += `<tbody id = "emp"><tr> <td> ${userList[i].id} </td> <td> ${userList[i].name} </td> <td> ${userList[i].username} </td> <td> ${userList[i].email} </td> 
+            <td> ${userList[i].company.name} </td> <td><button id = "edit-btn" onclick="editUserWindow(${userList[i].id})">Edit</button>&nbsp;&nbsp;<button id = "delete-btn" onclick = "deleteUser(${userList[i].id})">Delete</button> </td></tr></tbody>`;
         }
+        
         document.getElementById("userTable").innerHTML = list;
         
     }
     function editUserWindow(id){
         document.getElementById("editDiv").style.display = 'block';
         document.getElementById("userTable").style.display = 'none';
-        document.getElementById("id").value=id;
+        var UserDetailsEdit = EmpDetails[id-1]
+        document.getElementById("id").value=UserDetailsEdit.id;
+	    document.getElementById("name").value=UserDetailsEdit.name;
+	    document.getElementById("userName").value=UserDetailsEdit.username;
+	    document.getElementById("email").value=UserDetailsEdit.email;
+	    document.getElementById("company").value=UserDetailsEdit.company.name;
     }
-    function editUser(){
+    function SaveUser(){
         var id = document.getElementById("id").value; 
         var obj2 = {name: document.getElementById("company").value};
         var obj = {
@@ -54,11 +61,12 @@
         .then(json=>console.log(json))
     }
     function deleteUser(id){
-        fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id-1}`, {
     	method: 'DELETE'   
         })
-        EmpDetails.shift();
-        window.alert("Record Deleted Successfully")
+        EmpDetails.splice(id-1,1);
+        document.getElementById("msg").style.display = 'block';
+	    setTimeout(function(){document.getElementById("msg").style.display = 'none';},5000);
         Listitr(EmpDetails);
     }
     function reload(){
@@ -66,4 +74,59 @@
         document.getElementById("userTable").style.display = 'block';
         Listitr(EmpDetails);
     }
-    
+
+    function validateForm() {
+        var editName = document.forms["editForm"]["name"].value;
+        if (editName == "") {
+          alert("Name must be filled out");
+          return false;
+          
+        }
+
+        var userName = document.forms["editForm"]["userName"].value;
+        if (userName == "" || userName.length<8) {
+          alert("Username should be more than 8 characters");
+          return false;
+          
+        }
+
+        var email = document.forms["editForm"]["email"].value;
+        var atposition=email.indexOf("@");  
+        var dotposition=email.lastIndexOf(".");  
+        if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length){  
+            alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
+        return false;  
+        }  
+
+        var company = document.forms["editForm"]["company"].value;
+        if (company == "") {
+          alert("Company Name should be entered");
+          return false;
+          
+        }
+        SaveUser();
+      } 
+
+      function sortTable() { 
+        var table, i, first, second; 
+        table = document.getElementById("userTable"); 
+        var switching = true; 
+        while (switching) { 
+            switching = false; 
+            var rows = table.rows; 
+            for (i = 1; i < (rows.length - 1); i++) { 
+                var Switch = false; 
+                first = rows[i].getElementsByTagName("TD")[0]; 
+                second = rows[i + 1].getElementsByTagName("TD")[0]; 
+                if (first.innerHTML.toLowerCase() <= second.innerHTML.toLowerCase()) 
+                    { 
+                    Switch = true; 
+                    break; 
+                } 
+            } 
+            if (Switch) { 
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]); 
+                switching = true; 
+            } 
+        } 
+}
